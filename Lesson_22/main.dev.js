15b15ff71a7a19a43d0f38be73403cbfbfa59109
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2024-12-09 21:31:38 +0100

    Forgot to commit the rest

---
+++ Lesson_22/main.dev.js
@@ -0,0 +1,41 @@
+// Create & work with data
+// Create & work with html elements
+// Connect to the remote data (server)
+// Work with user's devices
+// Alex
+// const myName = "Sara";
+// const transformedName = myName.toUpperCase();
+// console.log(transformedName);
+// const a = 1;
+// const b = 2;
+// console.log(a + b);
+// // Global variable that can be used from anywhere
+// // var userName = "Alex";
+// // Local scope
+// if (10 > 9) {
+//     const userName = "Alex";
+//     // let userName = "Alex";
+//     // var userName = "Alex";
+// }
+// console.log(userName);
+// const userName = prompt("Are u okay?");
+// // const num1 = Number(prompt("Add the first number"));
+// const num1 = Number(prompt("Add the second number"));
+// // Can instead use parseInt. More reliable, will go character by character
+// const num2 = parseInt(prompt("Add the second number"));
+// const result = num1 + num2;
+// // To check the type you can use the typeof
+// console.log("Result is: " + result);
+// JS datatypes (can come up in an interview, how many datatypes we have)
+// number
+// string
+// boolean
+// undefined,
+// null, don't have a aprticular value
+// object
+// function
+// Will never use, ever!
+// bigint
+// symbol
+// Usually we are saying 6 primitives and 3 other, or you can go with 8 and skip function
+"use strict";
