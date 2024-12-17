6be1a718ea82b136647a4a4a2c2e26a4f1e02aa1
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2024-12-17 12:14:34 +0100

    Start of simple calculator

---
+++ Lesson_24/homework/main.dev.js
@@ -0,0 +1,72 @@
+"use strict";
+
+// const firstNum = +prompt("Enter first num: ");
+// const operator = prompt("Choose an operator");
+// const secondNum = +prompt("Enter second num: ");
+// switch (operator) {
+//     case "+":
+//         console.log(firstNum + secondNum);
+//         break;
+//     case "-":
+//         console.log(firstNum - secondNum);
+//         break;
+//     case "*":
+//         console.log(firstNum * secondNum);
+//         break;
+//     case "/":
+//         console.log(firstNum / secondNum);
+//         break;
+//     default:
+//         console.log("Bad expression");
+//         break;
+// }
+document.getElementById("calculate").onclick = function () {
+  var num1 = parseFloat(document.getElementById("first_input").value);
+  var operator = document.getElementById("operator_input").value;
+  var num2 = parseFloat(document.getElementById("second_input").value);
+
+  while (!num1) {
+    document.getElementById("totalOutput").textContent = "Please add a number";
+    console.log("Error");
+    break;
+  } // while (!num2) {
+  //     document.getElementById(
+  //         "totalOutput"
+  //     ).textContent = `Please add a number`;
+  //     console.log("Error");
+  // }
+  // while (
+  //     operator !== "+" &&
+  //     operator !== "-" &&
+  //     operator !== "*" &&
+  //     operator !== "/"
+  // ) {
+  //     document.getElementById(
+  //         "totalOutput"
+  //     ).textContent = `Please select an operator`;
+  //     console.log("Error");
+  // }
+
+
+  switch (operator) {
+    case "+":
+      console.log(num1 + num2);
+      break;
+
+    case "-":
+      console.log(num1 - num2);
+      break;
+
+    case "*":
+      console.log(num1 * num2);
+      break;
+
+    case "/":
+      console.log(num1 / num2);
+      break;
+
+    default:
+      console.log("Bad expression");
+      break;
+  }
+};
---
+++ Lesson_24/main.js
@@ -0,0 +1,123 @@
+// Data operators that automatically transform condition into a boolean type
+const a = 10 > 7;
+
+const userName = prompt("Please enter your name: ");
+
+console.log(userName.length);
+
+// if (userName) {
+//     console.log(`The username is ${userName}.`);
+// } else {
+//     console.log(`Please type in a valid user name`);
+// }
+
+// userName
+// userName.length >= 2
+
+if (userName && userName.length >= 2) {
+    console.log(`The username is ${userName}.`);
+} else {
+    console.log(`Please type in a valid user name`);
+}
+
+const side = prompt("Provide a square side");
+
+if (side > 0) {
+    const perimeter = side * 4;
+
+    if (true) {
+        console.log(perimeter);
+    }
+} else {
+    console.log("Side should be greater than 0");
+}
+
+const age = +prompt("Add your age"); //String will give you NaN
+
+console.log(age);
+
+if (isNaN(age) || age < 0) {
+    console.log("Valid number required");
+} else if (age > 0 && age <= 18) {
+    console.log("You are just a child");
+} else if (age > 18 && age <= 59) {
+    console.log("You are middle aged");
+} else {
+    console.log("You are experienced");
+}
+
+// 1-18 child
+// 18-59 middle age
+// 59+ experienced
+
+const dayNumber = +prompt("Provide a day number");
+
+if (isNaN(dayNumber) || dayNumber < 1 || dayNumber > 8) {
+    console.log("Monday");
+} else if (dayNumber === 1) {
+}
+
+switch (dayNumber) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    default:
+        console.log("Incorrect day");
+        break;
+}
+
+const properAnswer = 6;
+
+const userAnswer = +prompt("Add your number");
+
+if (userAnswer === properAnswer) {
+    console.log("It's correct");
+} else {
+    console.log("Try again");
+}
+
+const userNamePerson = promt("Add your name");
+
+// Basement for form validation logic
+if (userNamePerson) {
+    // Will transform it into a boolean. If it's false it will stop at the &&
+    userNamePerson && console.log("Hello", userNamePerson);
+}
+
+let gameName = prompt("Add you name");
+
+// if (!gameName) {
+//     gameName = prompt("Add you name");
+// }
+
+// Will be executed until true
+while (!gameName) {
+    gameName = prompt("Add you name");
+}
+
+// Homework Loops & condition - Create a mini calculator
+let num1 = +promt("Number1");
+let num2 = +promt("Number2");
+let action = +promt("Action");
+
+while (!num1) {
+    num1 = +promt("Number1");
+}
+
+while (!num2) {}
+
+while (action !== "+" && action !== "-" && action !== "/" && action !== "*") {
+    action = prompt("Action");
+}
+
+// Counting number back and forth (Optional)
+
+// Read up on the modulus
+// const num = a(456);
+// const num1 = a / 100;
+// const num2 = a (% 10 / 100)
+// const num3 = a % 10
