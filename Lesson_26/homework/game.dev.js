1292ff5760f1b5b68b9f591ac85a09094bf3edb2
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2024-12-29 16:41:25 +0100

    Loops and condition part 2 hw

--- Lesson_26/homework/game.dev.js
+++ Lesson_26/homework/game.dev.js
@@ -40,4 +40,133 @@ equalBtn.addEventListener("click", function () {
 });
 greaterThanBtn.addEventListener("click", function () {
   return updateGuessTheNumber(">");
+}); // Get user age function
+
+var resultButton = document.getElementById("age_submit");
+
+function getUserAge() {
+  var age = parseInt(document.getElementById("age_input").value);
+
+  if (isNaN(age) || age < 0) {
+    document.getElementById("age_group").textContent = "Invalid age";
+    console.log("Invalid age");
+    return;
+  }
+
+  switch (true) {
+    case age >= 0 && age <= 11:
+      document.getElementById("age_group").textContent = "You're a child";
+      console.log("You're a child");
+      break;
+
+    case age >= 12 && age <= 17:
+      document.getElementById("age_group").textContent = "You're a teenager";
+      console.log("Teenager");
+      break;
+
+    case age >= 18 && age <= 59:
+      document.getElementById("age_group").textContent = "You're an adult";
+      console.log("Adult");
+      break;
+
+    case age >= 60:
+      document.getElementById("age_group").textContent = "You're a pensioner";
+      console.log("Pensioner");
+      break;
+
+    default:
+      document.getElementById("age_group").textContent = "Invalid age";
+      console.log("Invalid age");
+  }
+}
+
+resultButton.addEventListener("click", getUserAge);
+var specialCharElement = document.getElementById("char_result");
+var zeroBtn = document.getElementById("zero");
+var oneBtn = document.getElementById("one");
+var twoBtn = document.getElementById("two");
+var threeBtn = document.getElementById("three");
+var fourBtn = document.getElementById("four");
+var fiveBtn = document.getElementById("five");
+var sixBtn = document.getElementById("six");
+var sevenBtn = document.getElementById("seven");
+var eightBtn = document.getElementById("eight");
+var nineBtn = document.getElementById("nine");
+
+function getSpecialCharacter(number) {
+  switch (true) {
+    case number == "0":
+      specialCharElement.textContent = "=";
+      break;
+
+    case number == "1":
+      specialCharElement.textContent = "!";
+      break;
+
+    case number == "2":
+      specialCharElement.textContent = "@";
+      break;
+
+    case number == "3":
+      specialCharElement.textContent = "#";
+      break;
+
+    case number == "4":
+      specialCharElement.textContent = "$";
+      break;
+
+    case number == "5":
+      specialCharElement.textContent = "%";
+      break;
+
+    case number == "6":
+      specialCharElement.textContent = "&";
+      break;
+
+    case number == "7":
+      specialCharElement.textContent = "/";
+      break;
+
+    case number == "8":
+      specialCharElement.textContent = "(";
+      break;
+
+    case number == "9":
+      specialCharElement.textContent = ")";
+      break;
+
+    default:
+      break;
+  }
+}
+
+zeroBtn.addEventListener("click", function () {
+  return getSpecialCharacter("0");
+});
+oneBtn.addEventListener("click", function () {
+  return getSpecialCharacter("1");
+});
+twoBtn.addEventListener("click", function () {
+  return getSpecialCharacter("2");
+});
+threeBtn.addEventListener("click", function () {
+  return getSpecialCharacter("3");
+});
+fourBtn.addEventListener("click", function () {
+  return getSpecialCharacter("4");
+});
+fiveBtn.addEventListener("click", function () {
+  return getSpecialCharacter("5");
+});
+sixBtn.addEventListener("click", function () {
+  return getSpecialCharacter("6");
+});
+sevenBtn.addEventListener("click", function () {
+  return getSpecialCharacter("7");
+});
+eightBtn.addEventListener("click", function () {
+  return getSpecialCharacter("8");
+});
+nineBtn.addEventListener("click", function () {
+  return getSpecialCharacter("9");
 });
--- Lesson_26/homework/game.js
+++ Lesson_26/homework/game.js
@@ -35,3 +35,107 @@ function updateGuessTheNumber(answer) {
 lessThanBtn.addEventListener("click", () => updateGuessTheNumber("<"));
 equalBtn.addEventListener("click", () => updateGuessTheNumber("=="));
 greaterThanBtn.addEventListener("click", () => updateGuessTheNumber(">"));
+
+// Get user age function
+const resultButton = document.getElementById("age_submit");
+
+function getUserAge() {
+    let age = parseInt(document.getElementById("age_input").value);
+
+    if (isNaN(age) || age < 0) {
+        document.getElementById("age_group").textContent = `Invalid age`;
+        console.log("Invalid age");
+        return;
+    }
+
+    switch (true) {
+        case age >= 0 && age <= 11:
+            document.getElementById("age_group").textContent = `You're a child`;
+            console.log("You're a child");
+            break;
+        case age >= 12 && age <= 17:
+            document.getElementById(
+                "age_group"
+            ).textContent = `You're a teenager`;
+            console.log("Teenager");
+            break;
+        case age >= 18 && age <= 59:
+            document.getElementById(
+                "age_group"
+            ).textContent = `You're an adult`;
+            console.log("Adult");
+            break;
+        case age >= 60:
+            document.getElementById(
+                "age_group"
+            ).textContent = `You're a pensioner`;
+            console.log("Pensioner");
+            break;
+        default:
+            document.getElementById("age_group").textContent = `Invalid age`;
+            console.log("Invalid age");
+    }
+}
+
+resultButton.addEventListener("click", getUserAge);
+
+const specialCharElement = document.getElementById("char_result");
+const zeroBtn = document.getElementById("zero");
+const oneBtn = document.getElementById("one");
+const twoBtn = document.getElementById("two");
+const threeBtn = document.getElementById("three");
+const fourBtn = document.getElementById("four");
+const fiveBtn = document.getElementById("five");
+const sixBtn = document.getElementById("six");
+const sevenBtn = document.getElementById("seven");
+const eightBtn = document.getElementById("eight");
+const nineBtn = document.getElementById("nine");
+
+function getSpecialCharacter(number) {
+    switch (true) {
+        case number == "0":
+            specialCharElement.textContent = `=`;
+            break;
+        case number == "1":
+            specialCharElement.textContent = `!`;
+            break;
+        case number == "2":
+            specialCharElement.textContent = `@`;
+            break;
+        case number == "3":
+            specialCharElement.textContent = `#`;
+            break;
+        case number == "4":
+            specialCharElement.textContent = `$`;
+            break;
+        case number == "5":
+            specialCharElement.textContent = `%`;
+            break;
+        case number == "6":
+            specialCharElement.textContent = `&`;
+            break;
+        case number == "7":
+            specialCharElement.textContent = `/`;
+            break;
+        case number == "8":
+            specialCharElement.textContent = `(`;
+            break;
+        case number == "9":
+            specialCharElement.textContent = `)`;
+            break;
+
+        default:
+            break;
+    }
+}
+
+zeroBtn.addEventListener("click", () => getSpecialCharacter("0"));
+oneBtn.addEventListener("click", () => getSpecialCharacter("1"));
+twoBtn.addEventListener("click", () => getSpecialCharacter("2"));
+threeBtn.addEventListener("click", () => getSpecialCharacter("3"));
+fourBtn.addEventListener("click", () => getSpecialCharacter("4"));
+fiveBtn.addEventListener("click", () => getSpecialCharacter("5"));
+sixBtn.addEventListener("click", () => getSpecialCharacter("6"));
+sevenBtn.addEventListener("click", () => getSpecialCharacter("7"));
+eightBtn.addEventListener("click", () => getSpecialCharacter("8"));
+nineBtn.addEventListener("click", () => getSpecialCharacter("9"));
