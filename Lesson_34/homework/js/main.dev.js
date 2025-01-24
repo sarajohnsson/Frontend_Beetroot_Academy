b121f4a1fabadeaaae1c35d23d31b4483e626742
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-01-24 18:02:55 +0100

    Beginning of homework

---
+++ Lesson_34/homework/js/main.dev.js
@@ -0,0 +1,51 @@
+"use strict";
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var tabs = _toConsumableArray(document.querySelectorAll(".tabs"));
+
+var contents = _toConsumableArray(document.querySelectorAll(".content"));
+
+tabs.forEach(function (tab) {
+  return tab.addEventListener("click", handleTabs);
+});
+var index = 0;
+
+function handleTabs(event) {
+  var currentElement = event.target.closest(".tabs");
+
+  if (!currentElement) {
+    console.log("Clicked element is not a tab");
+    return;
+  }
+
+  var newIndex = tabs.indexOf(currentElement);
+
+  if (newIndex === -1) {
+    console.log("Clicked element is not a tab");
+    return;
+  }
+
+  if (newIndex === index) {
+    console.log("Samt tab clicked again. Ignoring");
+    return;
+  }
+
+  if (index >= 0 && index < tabs.length) {
+    tabs[index].classList.remove("active");
+    contents[index].classList.remove("active");
+  }
+
+  index = newIndex;
+
+  if (index >= 0 && index < tabs.length) {
+    tabs[index].classList.add("active");
+    contents[index].classList.add("active");
+  }
+}
---
+++ Lesson_34/homework/js/main.js
@@ -0,0 +1,39 @@
+const tabs = [...document.querySelectorAll(".tabs")];
+const contents = [...document.querySelectorAll(".content")];
+
+tabs.forEach((tab) => tab.addEventListener("click", handleTabs));
+
+let index = 0;
+
+function handleTabs(event) {
+    const currentElement = event.target.closest(".tabs");
+
+    if (!currentElement) {
+        console.log("Clicked element is not a tab");
+        return;
+    }
+
+    const newIndex = tabs.indexOf(currentElement);
+
+    if (newIndex === -1) {
+        console.log("Clicked element is not a tab");
+        return;
+    }
+
+    if (newIndex === index) {
+        console.log("Samt tab clicked again. Ignoring");
+        return;
+    }
+
+    if (index >= 0 && index < tabs.length) {
+        tabs[index].classList.remove("active");
+        contents[index].classList.remove("active");
+    }
+
+    index = newIndex;
+
+    if (index >= 0 && index < tabs.length) {
+        tabs[index].classList.add("active");
+        contents[index].classList.add("active");
+    }
+}
