74eac95ea70682770d0fb7e07af6a2247dcce1df
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-01-27 15:36:20 +0100

    Homework done

--- Lesson_34/homework/js/main.dev.js
+++ Lesson_34/homework/js/main.dev.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
@@ -8,44 +10,61 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
-var tabs = _toConsumableArray(document.querySelectorAll(".tabs"));
+var tabs = _toConsumableArray(document.querySelectorAll('.tabs'));
 
-var contents = _toConsumableArray(document.querySelectorAll(".content"));
+var contents = _toConsumableArray(document.querySelectorAll('.content'));
 
 tabs.forEach(function (tab) {
-  return tab.addEventListener("click", handleTabs);
+  return tab.addEventListener('click', handleTabs);
 });
 var index = 0;
 
 function handleTabs(event) {
-  var currentElement = event.target.closest(".tabs");
+  var currentElement = event.target.closest('.tabs');
 
   if (!currentElement) {
-    console.log("Clicked element is not a tab");
+    console.log('Clicked element is not a tab');
     return;
   }
 
   var newIndex = tabs.indexOf(currentElement);
 
   if (newIndex === -1) {
-    console.log("Clicked element is not a tab");
+    console.log('Clicked element is not a tab');
     return;
   }
 
   if (newIndex === index) {
-    console.log("Same tab clicked again. Ignoring");
+    console.log('Same tab clicked again. Ignoring');
     return;
   }
 
   if (index >= 0 && index < tabs.length) {
-    tabs[index].classList.remove("active");
-    contents[index].classList.remove("active_content");
+    tabs[index].classList.remove('active');
+    contents[index].classList.remove('active_content');
   }
 
   index = newIndex;
 
   if (index >= 0 && index < tabs.length) {
-    tabs[index].classList.add("active");
-    contents[index].classList.add("active_content");
+    tabs[index].classList.add('active');
+    contents[index].classList.add('active_content');
   }
 }
+
+radialObj = radialIndicator('.indicatorContainer', {
+  radius: 40,
+  barColor: _defineProperty({
+    0: '#00D4BE',
+    19: '#1AE780',
+    83: '#1AE780'
+  }, "0", '#00D4BE'),
+  barWidth: 5,
+  initValue: 8.7,
+  minValue: 0,
+  maxValue: 10,
+  roundCorner: true,
+  percentage: false,
+  interpolate: true,
+  format: '##.#'
+});
--- Lesson_34/homework/js/main.js
+++ Lesson_34/homework/js/main.js
@@ -1,39 +1,57 @@
-const tabs = [...document.querySelectorAll(".tabs")];
-const contents = [...document.querySelectorAll(".content")];
+const tabs = [...document.querySelectorAll('.tabs')];
+const contents = [...document.querySelectorAll('.content')];
 
-tabs.forEach((tab) => tab.addEventListener("click", handleTabs));
+tabs.forEach((tab) => tab.addEventListener('click', handleTabs));
 
 let index = 0;
 
 function handleTabs(event) {
-    const currentElement = event.target.closest(".tabs");
+    const currentElement = event.target.closest('.tabs');
 
     if (!currentElement) {
-        console.log("Clicked element is not a tab");
+        console.log('Clicked element is not a tab');
         return;
     }
 
     const newIndex = tabs.indexOf(currentElement);
 
     if (newIndex === -1) {
-        console.log("Clicked element is not a tab");
+        console.log('Clicked element is not a tab');
         return;
     }
 
     if (newIndex === index) {
-        console.log("Same tab clicked again. Ignoring");
+        console.log('Same tab clicked again. Ignoring');
         return;
     }
 
     if (index >= 0 && index < tabs.length) {
-        tabs[index].classList.remove("active");
-        contents[index].classList.remove("active_content");
+        tabs[index].classList.remove('active');
+        contents[index].classList.remove('active_content');
     }
 
     index = newIndex;
 
     if (index >= 0 && index < tabs.length) {
-        tabs[index].classList.add("active");
-        contents[index].classList.add("active_content");
+        tabs[index].classList.add('active');
+        contents[index].classList.add('active_content');
     }
 }
+
+radialObj = radialIndicator('.indicatorContainer', {
+    radius: 40,
+    barColor: {
+        0: '#00D4BE',
+        19: '#1AE780',
+        83: '#1AE780',
+        0: '#00D4BE',
+    },
+    barWidth: 5,
+    initValue: 8.7,
+    minValue: 0,
+    maxValue: 10,
+    roundCorner: true,
+    percentage: false,
+    interpolate: true,
+    format: '##.#',
+});
