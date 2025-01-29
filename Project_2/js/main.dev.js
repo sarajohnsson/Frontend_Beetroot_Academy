cc86a273a41c7283f1771ca6707d9d5538d7cc05
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-01-29 16:30:47 +0100

    Slider beginning

--- Project_2/js/main.dev.js
+++ Project_2/js/main.dev.js
@@ -2,12 +2,24 @@
 
 $(document).ready(function () {
   // Slick slider
-  $('.vertical_carousel').slick({
+  $('.vertical_slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    dots: true,
     vertical: true,
-    verticalSwipe: true
+    verticalSwiping: true,
+    speed: 800,
+    dots: true,
+    prevArrow: '',
+    nextArrow: ''
+  });
+  $('.horizontal_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    speed: 800,
+    dots: true,
+    prevArrow: '<i class="fa-solid fa-chevron-left left_arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right right_arrow"></i>'
   });
 });
