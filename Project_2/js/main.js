3560d6b47f5f9f640e31adfa37f975e944acfe16
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-01-31 16:04:03 +0100

    Adjusment slider

--- Project_2/js/main.js
+++ Project_2/js/main.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
     });
 
     $('.horizontal_slider').slick({
+        initialSlide: 0,
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
@@ -20,5 +21,33 @@ $(document).ready(function () {
         dots: true,
         prevArrow: '<i class="fa-solid fa-chevron-left left_arrow"></i>',
         nextArrow: '<i class="fa-solid fa-chevron-right right_arrow"></i>',
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
     });
 });
