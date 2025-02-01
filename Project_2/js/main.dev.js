d5e255a92d6adb0297ec74c73c656d7ac0af94dd
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-02-01 16:47:29 +0100

    Hero section ongoing

--- Project_2/js/main.dev.js
+++ Project_2/js/main.dev.js
@@ -1,5 +1,28 @@
 "use strict";
 
+// Calculate the height of the tallest slide
+// Set that height to all other slides
+// - Select all slides
+// - Loop through the selected slides, no matter how many containers
+// - Store the elements in a variable
+// - Find the tallest slide
+// - Set height to maximum height
+// - Dynamically calculate height
+function setEqualHeight() {
+  var slides = document.querySelectorAll('.slider_testimonials');
+  var maxHeight = 0;
+  slides.forEach(function (slide) {
+    slide.style.height = 'auto';
+
+    if (slide.offsetHeight > maxHeight) {
+      maxHeight = slide.offsetHeight;
+    }
+
+    slide.style.height = "".concat(maxHeight, "px");
+  });
+} // SLICK SLIDER
+
+
 $(document).ready(function () {
   // Slick slider
   $('.vertical_slider').slick({
@@ -17,16 +40,16 @@ $(document).ready(function () {
     initialSlide: 0,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     speed: 800,
     dots: true,
     prevArrow: '<i class="fa-solid fa-chevron-left left_arrow"></i>',
     nextArrow: '<i class="fa-solid fa-chevron-right right_arrow"></i>',
     responsive: [{
-      breakpoint: 1280,
+      breakpoint: 1024,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         infinite: true,
         dots: true
       }
@@ -42,9 +65,14 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1
       }
-    } // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ]
+    }]
   });
+  window.addEventListener('load', setEqualHeight);
+  window.addEventListener('resize', setEqualHeight);
+  $('.horizontal_slider').on('init', setEqualHeight);
+  $('.horizontal_slider').on('setPosition', setEqualHeight);
+}); // LIGHTGALLERY
+
+lightGallery(document.getElementById('lightgallery'), {
+  plugins: [lgZoom, lgFullscreen]
 });
--- Project_2/js/main.js
+++ Project_2/js/main.js
@@ -1,3 +1,28 @@
+// Calculate the height of the tallest slide
+// Set that height to all other slides
+// - Select all slides
+// - Loop through the selected slides, no matter how many containers
+// - Store the elements in a variable
+// - Find the tallest slide
+// - Set height to maximum height
+// - Dynamically calculate height
+
+function setEqualHeight() {
+    const slides = document.querySelectorAll('.slider_testimonials');
+    let maxHeight = 0;
+
+    slides.forEach((slide) => {
+        slide.style.height = 'auto';
+
+        if (slide.offsetHeight > maxHeight) {
+            maxHeight = slide.offsetHeight;
+        }
+
+        slide.style.height = `${maxHeight}px`;
+    });
+}
+
+// SLICK SLIDER
 $(document).ready(function () {
     // Slick slider
     $('.vertical_slider').slick({
@@ -16,17 +41,18 @@ $(document).ready(function () {
         initialSlide: 0,
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         speed: 800,
         dots: true,
+
         prevArrow: '<i class="fa-solid fa-chevron-left left_arrow"></i>',
         nextArrow: '<i class="fa-solid fa-chevron-right right_arrow"></i>',
         responsive: [
             {
-                breakpoint: 1280,
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true,
                 },
@@ -45,9 +71,17 @@ $(document).ready(function () {
                     slidesToScroll: 1,
                 },
             },
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
         ],
     });
+
+    window.addEventListener('load', setEqualHeight);
+    window.addEventListener('resize', setEqualHeight);
+
+    $('.horizontal_slider').on('init', setEqualHeight);
+    $('.horizontal_slider').on('setPosition', setEqualHeight);
+});
+
+// LIGHTGALLERY
+lightGallery(document.getElementById('lightgallery'), {
+    plugins: [lgZoom, lgFullscreen],
 });
