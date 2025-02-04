3472a9fda85db48738fd75856e610b9ed41f0285
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-02-04 17:44:47 +0100

    Small changes top Project 2

--- Lesson_30/pokemon/pokeApi.dev.js
+++ Lesson_30/pokemon/pokeApi.dev.js
@@ -4,53 +4,54 @@
 // Display the info and the image
 // Display the name, stats, attacks etc in a Pokemon card
 // const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-var pokemonInput = document.querySelector(".pokemon_container_search_input");
-var buttonElement = document.querySelector(".pokemon_container_search_btn");
-var pokemonName = document.querySelector(".pokemon_card_bg_info_name");
-var pokemonHp = document.querySelector(".pokemon_card_bg_info_hp_text");
-var pokemonType = document.querySelector(".pokemon_card_bg_stats_type");
-var pokemonAttack = document.querySelector(".pokemon_card_bg_stats_attack_name");
-var pokemonDamage = document.querySelector(".pokemon_card_bg_stats_attack_dmg");
-var pokemonNr = document.querySelector(".pokemon_card_bg_stats_gen_id");
-var pokemonWeight = document.querySelector(".pokemon_card_bg_stats_gen_weight");
-var imgElement = document.querySelector(".pokemon_card_bg_img_sprite");
+var pokemonInput = document.querySelector('.pokemon_container_search_input');
+var buttonElement = document.querySelector('.pokemon_container_search_btn');
+var pokemonName = document.querySelector('.pokemon_card_bg_info_name');
+var pokemonHp = document.querySelector('.pokemon_card_bg_info_hp_text');
+var pokemonType = document.querySelector('.pokemon_card_bg_stats_type');
+var pokemonAttack = document.querySelector('.pokemon_card_bg_stats_attack_name');
+var pokemonDamage = document.querySelector('.pokemon_card_bg_stats_attack_dmg');
+var pokemonNr = document.querySelector('.pokemon_card_bg_stats_gen_id');
+var pokemonWeight = document.querySelector('.pokemon_card_bg_stats_gen_weight');
+var imgElement = document.querySelector('.pokemon_card_bg_img_sprite');
 var typeColors = {
-  normal: "#929da2",
-  poison: "#ab6bc8",
-  bug: "#91c12f",
-  fire: "#ff9c54",
-  electric: "#f4d339",
-  dragon: "#086dc3",
-  fighting: "#cd416a",
-  ground: "#d97845",
-  ghost: "#516aae",
-  water: "#4f90d6",
-  psychic: "#fa7077",
-  dark: "#5a5465",
-  flying: "#90a8de",
-  rock: "#c5b68c",
-  steel: "#598ea1",
-  grass: "#63bd5a",
-  ice: "#73cebe",
-  fairy: "#ed8fe7"
+  normal: '#929da2',
+  poison: '#ab6bc8',
+  bug: '#91c12f',
+  fire: '#ff9c54',
+  electric: '#f4d339',
+  dragon: '#086dc3',
+  fighting: '#cd416a',
+  ground: '#d97845',
+  ghost: '#516aae',
+  water: '#4f90d6',
+  psychic: '#fa7077',
+  dark: '#5a5465',
+  flying: '#90a8de',
+  rock: '#c5b68c',
+  steel: '#598ea1',
+  grass: '#63bd5a',
+  ice: '#73cebe',
+  fairy: '#ed8fe7'
 };
+var favButton = document.querySelector('.fav_btn');
 
 function getRandomPokemonId() {
   return Math.floor(Math.random() * 1000) + 1;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   var randomPokemonId = getRandomPokemonId();
   getPokemon(randomPokemonId);
 });
-buttonElement.addEventListener("click", function () {
+buttonElement.addEventListener('click', function () {
   var pokemonId = pokemonInput.value.toLowerCase();
 
   if (pokemonId) {
     getPokemon(pokemonId);
   } else {
-    console.log("Please enter a Pokemon name!");
-    alert("Please enter a Pokemon name!");
+    console.log('Please enter a Pokemon name!');
+    alert('Please enter a Pokemon name!');
   }
 });
 
@@ -58,7 +59,7 @@ function getPokemon(pokemonId) {
   var url = "https://pokeapi.co/api/v2/pokemon/".concat(pokemonId);
   fetch(url).then(function (response) {
     if (!response.ok) {
-      throw new Error("Pokemon not found!");
+      throw new Error('Pokemon not found!');
     }
 
     return response.json();
@@ -73,7 +74,7 @@ function getPokemon(pokemonId) {
 function displayPokemonCard(data) {
   var pokemonSprite = data.sprites.other.dream_world.front_default || data.sprites.front_default;
   imgElement.src = pokemonSprite;
-  imgElement.style.display = "block";
+  imgElement.style.display = 'block';
   pokemonName.textContent = data.name;
   pokemonHp.textContent = data.stats[0].base_stat;
   pokemonAttack.textContent = data.abilities[0].ability.name;
@@ -85,12 +86,18 @@ function displayPokemonCard(data) {
 
 function getPokemonType(pokemonData) {
   var type = pokemonData.types[0].type.name;
-  var pokemonCardBg = document.querySelector(".pokemon_card_bg");
+  var pokemonCardBg = document.querySelector('.pokemon_card_bg');
   pokemonType.textContent = pokemonData.types[0].type.name;
 
   if (typeColors[type]) {
-    pokemonCardBg.style.setProperty("--normal-color", typeColors[type]);
+    pokemonCardBg.style.setProperty('--normal-color', typeColors[type]);
   } else {
-    pokemonCardBg.style.setProperty("--normal-color", typeColors.normal);
+    pokemonCardBg.style.setProperty('--normal-color', typeColors.normal);
   }
+}
+
+favButton.addEventListener('click', savePokemon);
+
+function savePokemon() {
+  var currentFav = localStorage.get();
 }
--- Lesson_30/pokemon/pokeApi.js
+++ Lesson_30/pokemon/pokeApi.js
@@ -3,62 +3,58 @@
 // Display the name, stats, attacks etc in a Pokemon card
 
 // const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-const pokemonInput = document.querySelector(".pokemon_container_search_input");
-const buttonElement = document.querySelector(".pokemon_container_search_btn");
+const pokemonInput = document.querySelector('.pokemon_container_search_input');
+const buttonElement = document.querySelector('.pokemon_container_search_btn');
 
-const pokemonName = document.querySelector(".pokemon_card_bg_info_name");
-const pokemonHp = document.querySelector(".pokemon_card_bg_info_hp_text");
-const pokemonType = document.querySelector(".pokemon_card_bg_stats_type");
-const pokemonAttack = document.querySelector(
-    ".pokemon_card_bg_stats_attack_name"
-);
-const pokemonDamage = document.querySelector(
-    ".pokemon_card_bg_stats_attack_dmg"
-);
-const pokemonNr = document.querySelector(".pokemon_card_bg_stats_gen_id");
-const pokemonWeight = document.querySelector(
-    ".pokemon_card_bg_stats_gen_weight"
-);
-const imgElement = document.querySelector(".pokemon_card_bg_img_sprite");
+const pokemonName = document.querySelector('.pokemon_card_bg_info_name');
+const pokemonHp = document.querySelector('.pokemon_card_bg_info_hp_text');
+const pokemonType = document.querySelector('.pokemon_card_bg_stats_type');
+const pokemonAttack = document.querySelector('.pokemon_card_bg_stats_attack_name');
+const pokemonDamage = document.querySelector('.pokemon_card_bg_stats_attack_dmg');
+const pokemonNr = document.querySelector('.pokemon_card_bg_stats_gen_id');
+const pokemonWeight = document.querySelector('.pokemon_card_bg_stats_gen_weight');
+const imgElement = document.querySelector('.pokemon_card_bg_img_sprite');
 
 const typeColors = {
-    normal: "#929da2",
-    poison: "#ab6bc8",
-    bug: "#91c12f",
-    fire: "#ff9c54",
-    electric: "#f4d339",
-    dragon: "#086dc3",
-    fighting: "#cd416a",
-    ground: "#d97845",
-    ghost: "#516aae",
-    water: "#4f90d6",
-    psychic: "#fa7077",
-    dark: "#5a5465",
-    flying: "#90a8de",
-    rock: "#c5b68c",
-    steel: "#598ea1",
-    grass: "#63bd5a",
-    ice: "#73cebe",
-    fairy: "#ed8fe7",
+    normal: '#929da2',
+    poison: '#ab6bc8',
+    bug: '#91c12f',
+    fire: '#ff9c54',
+    electric: '#f4d339',
+    dragon: '#086dc3',
+    fighting: '#cd416a',
+    ground: '#d97845',
+    ghost: '#516aae',
+    water: '#4f90d6',
+    psychic: '#fa7077',
+    dark: '#5a5465',
+    flying: '#90a8de',
+    rock: '#c5b68c',
+    steel: '#598ea1',
+    grass: '#63bd5a',
+    ice: '#73cebe',
+    fairy: '#ed8fe7',
 };
+
+const favButton = document.querySelector('.fav_btn');
 
 function getRandomPokemonId() {
     return Math.floor(Math.random() * 1000) + 1;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     const randomPokemonId = getRandomPokemonId();
     getPokemon(randomPokemonId);
 });
 
-buttonElement.addEventListener("click", () => {
+buttonElement.addEventListener('click', () => {
     const pokemonId = pokemonInput.value.toLowerCase();
 
     if (pokemonId) {
         getPokemon(pokemonId);
     } else {
-        console.log("Please enter a Pokemon name!");
-        alert("Please enter a Pokemon name!");
+        console.log('Please enter a Pokemon name!');
+        alert('Please enter a Pokemon name!');
     }
 });
 
@@ -68,7 +64,7 @@ function getPokemon(pokemonId) {
     fetch(url)
         .then((response) => {
             if (!response.ok) {
-                throw new Error("Pokemon not found!");
+                throw new Error('Pokemon not found!');
             }
             return response.json();
         })
@@ -82,12 +78,10 @@ function getPokemon(pokemonId) {
 }
 
 function displayPokemonCard(data) {
-    const pokemonSprite =
-        data.sprites.other.dream_world.front_default ||
-        data.sprites.front_default;
+    const pokemonSprite = data.sprites.other.dream_world.front_default || data.sprites.front_default;
 
     imgElement.src = pokemonSprite;
-    imgElement.style.display = "block";
+    imgElement.style.display = 'block';
 
     pokemonName.textContent = data.name;
     pokemonHp.textContent = data.stats[0].base_stat;
@@ -101,12 +95,18 @@ function displayPokemonCard(data) {
 
 function getPokemonType(pokemonData) {
     const type = pokemonData.types[0].type.name;
-    const pokemonCardBg = document.querySelector(".pokemon_card_bg");
+    const pokemonCardBg = document.querySelector('.pokemon_card_bg');
     pokemonType.textContent = pokemonData.types[0].type.name;
 
     if (typeColors[type]) {
-        pokemonCardBg.style.setProperty("--normal-color", typeColors[type]);
+        pokemonCardBg.style.setProperty('--normal-color', typeColors[type]);
     } else {
-        pokemonCardBg.style.setProperty("--normal-color", typeColors.normal);
+        pokemonCardBg.style.setProperty('--normal-color', typeColors.normal);
     }
+}
+
+favButton.addEventListener('click', savePokemon);
+
+function savePokemon() {
+    const currentFav = localStorage.get();
 }
